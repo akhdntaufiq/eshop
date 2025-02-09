4ce07f5a5abce220c4b75a9370006d25484aaa75
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2025-02-10 06:34:18 +0700

    feat: add Product model class with basic fields

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private String productID;
+    private double productName;
+    private int productQuantity;
+}
