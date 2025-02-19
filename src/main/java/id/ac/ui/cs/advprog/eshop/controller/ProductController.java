d3d54ab140cd2fba2562c7eee6856e6c429ca167
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2025-02-19 16:10:30 +0700

    fix unused local variables "product1" in ProductController.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -38,7 +38,7 @@ public class ProductController {
 
     @PostMapping("/edit/{productId}")
     public String editProductPost(@ModelAttribute Product product, @PathVariable("productId") String productID, Model model) {
-        Product product1 = service.edit(product, productID);
+        service.edit(product, productID);
         return "redirect:/product/list";
     }
 
